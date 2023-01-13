d29507bb72380b2156118da6d7e2d4fafdd7f26b
Frad LEE <fradser@gmail.com>
2023-01-14 02:26:24 +0800

    feat(dock): add view-in animation

--- Assets/Scenes/MoveGaze.cs
+++ Assets/Scenes/MoveGaze.cs
@@ -7,6 +7,7 @@ public class MoveGaze: MonoBehaviour {
   private float startTime;
   private GameObject attachedObject = null;
   private GameObject lookedatObject = null;
+  private bool dockHasMovedForward = false;
   
   // Start is called before the first frame update
   void Start() {
@@ -17,26 +18,24 @@ public class MoveGaze: MonoBehaviour {
   void Update() {
     Ray ray = new Ray(transform.position, transform.forward);
     RaycastHit hit;
+    
     float currentTime = Time.time;
 
     if (attachedObject == null) {
       // Check to see if the ray cast from the CenterEyeObject hit anything
       if (Physics.Raycast(ray, out hit, 100)) {
-        // If the ray cast has hit something check to see if it has been for longer than 2 seconds
-        if ((currentTime - startTime) > 2) {
+        // If the ray cast has hit something check to see if it has been for longer than 0.2 seconds
+        if ((currentTime - startTime) > 0.2) {
           attachedObject = lookedatObject;
-          attachedObject.GetComponent < Rigidbody > ().useGravity = false;
-          attachedObject.GetComponent < Rigidbody > ().isKinematic = false;
-          // Set the position of the object to the position of the CenterEyeObject
-          // attachedObject.transform.parent = this.transform;
-          attachedObject.GetComponent < Renderer > ().material.color = Color.red;
-          // Trigger the animation
-          attachedObject.GetComponent < Animator > ().SetTrigger("Sight Into");
+          dockState = 2;
+          if (dockHasMovedForward == false) {
+            attachedObject.GetComponent < Animator > ().SetTrigger("Sight Into");
+            dockHasMovedForward = true;
+          }
         } else {
-          // If less than 2 seconds make it the looked at object and turn it blue
+          // If less than 0.2 seconds make it the looked at object
           if (lookedatObject == null) {
             lookedatObject = hit.collider.gameObject;
-            lookedatObject.GetComponent < Renderer > ().material.color = Color.blue;
           }
         }
       } else {
